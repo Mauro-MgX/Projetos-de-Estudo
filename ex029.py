581903b52abf3c41c3780b97243ec2f76b6aff37
Mauro <mauromartinsmgx@gmail.com>
2025-02-01 13:01:07 -0300

    Atualização do Exercício

--- ex029.py
+++ ex029.py
@@ -2,12 +2,21 @@
 # Se ele ultrapássar 80km/h, mostre uma mensagem dizendo que ele foi multado.
 # A multa vai custar R$7,00 por Km acima do limite
 
+from time import sleep
+
 km = int(input('Digite quantos KM seu carro passou na via ?'))
 
+print('Sua velocidade é de {} km'.format(km))
+
+print("Processando!")
+sleep(3)
+
 if km > 80 :
-    print('Seu carro foi multado!')
+    print('Você foi multado!')
     km_max = km - 80
     valor_multa = km_max * 7
+    print('Você passou {} km a mais do permitido'.format(km_max))
     print('A multa foi de {:.2f} !'.format(valor_multa))
 else:
     print('Você estava no limite permitido!')    
+print('Tenha uma boa viagem!')    
--- ex030.py
+++ ex030.py
@@ -1,11 +1,22 @@
 # Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou IMPAR.
 
+from time import sleep
+
+print('Olá mais sou eu Bot!')
+
+print('Hoje que mostrar que consigo descobrir se um número é PAR ou IMPAR')
+
 num = int(input('Digite um número: '))
 
 resto = num % 2
+
+print('Estou pensando...')
+
+sleep(3)
 
 if resto == 0:
     print('O número digitado foi PAR!')
 else:
     print('O numero digitado foi IMPAR!')    
 
+print('Obrigado por particiar dessa brincadeira!')
