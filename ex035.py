7270b9c8c22d8fa82936e4169cddcc53b546a05d
Mauro <mauromartinsmgx@gmail.com>
2025-02-01 17:20:37 -0300

    ajuste na logica
    logica foi ajustada.

--- ex035.py
+++ ex035.py
@@ -4,13 +4,7 @@ lado2 = float(input('Digite o segunda reta: '))
 lado3 = float(input('Digite o terceira reta: '))
 
 
-if lado1 + lado2 > lado3 :
+if lado1 < lado2 + lado3 and lado2 < lado1 + lado3 and lado3 < lado1 + lado2 :
     print('As 3 retas podem formar um triangulo')
 else:
-    if lado1 + lado3 > lado2 :
-        print('As 3 retas podem formar um triangulo')
-    else:
-        if lado2 + lado3 > lado1 :
-                    print('As 3 retas podem formar um triangulo')
-        else:
-              print('As 3 retas não podem formar um triangulo')
+    print('As 3 retas não podem formar um triangulo')    
