96fd0b50149b2a3360c7adbe3df2b3f1048e9501
Mauro <mauromartinsmgx@gmail.com>
2025-02-01 13:28:09 -0300

    Erro na logica foram corrigidos.

--- ex032.py
+++ ex032.py
@@ -1,15 +1,12 @@
 # Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO
 
-ano = int(input('Digite um ano qual quer: '))
+from datetime import date
 
-if ano // 4 == 0 :
-    if ano // 100 == 0 :
-        print('O ano digitado {} não é Bissexto!'.format(ano))
-    else:
-      
+ano = int(input('Digite um ano qual para analisar, caso queira analisar o ano atual digite 0: '))
+
+if ano == 0 :
+    ano = date.today().year
+if ano % 4 == 0 and  ano % 100 != 0 or ano % 400 == 0 :
         print('O ano digitado {} é Bissexto!'.format(ano))  
 else:
-    if ano // 400 == 0:
-        print('O ano digitado {} é Bissexto!'.format(ano))
-    else:
-        print('O ano digitado {} não é Bissexto!'.format(ano))
+    print('O ano digitado {} não é Bissexto!'.format(ano))
